0b50b44f9f3abe20c8bba8701a5e9fa9c8da9fab
Stewart Robinson <stew@fullfatthings.com>
2019-05-02 22:09:12 +0100

    This creates strings that comply with ISO8601
    With hours and minutes in the string passed in as 24 hours before today (with minutes e.t.c.)
    
    This caused a response error about an invalidate date format. This no longer happens with this
    
    ```
    $date->add(DateInterval::createFromDateString('yesterday'));
    echo $date->format('F j, Y') . "\n";
    $timeEntries = $client->timeEntries()->all(['updated_since' =>  $date]);
    ```

--- src/Api/TimeEntries.php
+++ src/Api/TimeEntries.php
@@ -39,7 +39,7 @@ class TimeEntries extends AbstractApi {
 	 */
 	public function all( array $parameters = [] ) {
 		if ( isset( $parameters['updated_since'] ) && $parameters['updated_since'] instanceof DateTime ) {
-			$parameters['updated_since'] = $parameters['updated_since']->format( 'Y-m-d H:i' );
+			$parameters['updated_since'] = $parameters['updated_since']->format(DateTime::ATOM);
 		}
 
 		if ( isset( $parameters['from'] ) && $parameters['from'] instanceof DateTime ) {
